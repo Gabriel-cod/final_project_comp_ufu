e68f1f34ef0abfc2d5f3582f97a74dda955c8ee0
Gabriel Alves Silva <galvessilva@win.ufu.br>
2026-02-27 20:39:50 -0300

    add base html and listview to missions

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mission.views import MissionListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MissionListView.as_view(), name='list_missions'),
 ]
